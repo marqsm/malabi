d238464dd6df02a234bacb992d9ab4ce9cfa626e
mmalka <marcus.malka@sujuwa.fi>
2014-01-05 23:57:37 +0200

    Initial commit.

---
+++ malabi.js
@@ -0,0 +1,237 @@
+/**
+ * Malabi - utility library made for learning purposes.
+ *
+ * Inspiration: underscore, lo-dash, allong.es
+ * PartialLeft & PartialRight and a lot of ideas and inspiration from Reginald Braithwaites excellent eBook "JavaScript Allongé"
+ */
+ (function(context) {
+
+    oldMalabi = context.malabi;
+    context.malabi = {};
+
+    // Cached functions
+    var __slice = Array.prototype.slice;
+
+    // Constantish
+    var __breaker = null;
+
+    malabi.compose = function(a, b) {
+        return function(c) {
+            //var args = toArray(arguments);
+            return a(b(c));
+        };
+    };
+
+    var toArray = malabi.toArray = function() {
+        return Array.prototype.slice.call(arguments);
+    };
+
+    malabi.partialLeft = function(fn, larg) {
+        return function() {
+            var args = __slice.call(arguments, 0);
+            return fn.apply(this, [larg].concat(args));
+        };
+    };
+
+    malabi.partialRight = function(fn, rarg) {
+        return function() {
+            var args = __slice.call(arguments, 0);
+            return fn.apply(this, args.concat(rarg));
+        };
+    };
+
+
+    malabi.curry = function(a, b) {
+        return function() {
+            return false;
+        };
+    };
+
+    malabi.range = function(end) {
+        var arr = [];
+
+        for (i=1; i<=end; i++) {
+            arr.push(i);
+        }
+        return arr;
+    };
+
+    // usage foreach(set, function(value, index) { return 'new_value'; })
+    var foreach = malabi.foreach = function(set, func) {
+        for (var i = 0; i < set.length; i++) {
+            func(set[i], i);
+        }
+    };
+
+    malabi.filter = function(set, filterFunc) {
+        var res = [];
+        foreach(set, function(item) {
+            if (filterFunc(item)) {
+                res.push(item);
+            }
+        });
+        return res;
+    };
+
+    malabi.map = function(set, func) {
+        var arr = [];
+        foreach(set, function(value, index) {
+            arr.push(func(value, index));
+        });
+        return arr;
+    };
+
+    malabi.pluck = function(set, propertyName) {
+        var arr = [];
+        foreach(set, function(value) {
+            arr.push(value[propertyName]);
+        });
+        return arr;
+    };
+
+    // TODO: stop iterating if value found
+    malabi.contains = function(set, matchValue) {
+        var hasValue = false;
+        foreach(set, function(value) {
+            if (value === matchValue) {
+                hasValue = true;
+            }
+        });
+        return hasValue;
+    }
+
+
+    // TEST: reduce([1, 2, 3, 4, 5], function(sum, curr) { return sum + curr}) == 15
+    // TEST: reduce([[0, 1], [2, 3], [4, 5]], function(a, b) { return a.concat(b); }) isEqual [0, 1, 2, 3, 4, 5]
+    var reduce = malabi.reduce = function(set, func, optInitialValue) {
+        var sum = optInitialValue;
+        if (isUndefined(optInitialValue) && isArray(set[0])) {
+            sum = [];
+        } else {
+            sum = null;
+        }
+
+        for (var index=0; index < set.length; index++) {
+            sum = func(sum, set[index], index, set);
+        }
+        return sum;
+    };
+
+    /**
+     * Array functions
+     */
+     // TODO: flattens nested arrays
+    var flatten = malabi.flatten = function(arr) {
+        var flat = arr.reduce(function(a, b) {
+            if (Array.isArray(b)) {
+                b = flatten(b);
+            }
+            return a.concat(b)
+        }, []);
+        return flat;
+    };
+
+    malabi.first = function(set) {
+        if (isArray(set)) {
+            if (set.length > 0) {
+                return set[0];
+            }
+            return [];
+        } else if (isString(set)) {
+            if (set.length > 0) {
+                return set.slice(0, 1);
+            }
+            return '';
+        }
+        return null;
+    };
+
+    malabi.rest = function(set) {
+        if (isArray(set)) {
+            if (set.length > 1) {
+                return set.splice(1);
+            }
+            return [];
+        } else if (isString(set)) {
+            if (set.length > 1) {
+                return set.slice(1);
+            }
+            return '';
+        }
+        return null;
+     };
+
+     malabi.last = function(set) {
+        if (isArray(set)) {
+            if (set.length > 0) {
+                return set[set.length - 1];
+            }
+            return [];
+        } else if (isString(set)) {
+            if (set.length > 0) {
+                return set.slice(-1);
+            }
+            return '';
+        }
+        return [];
+     };
+
+
+
+    /**
+     * Type checks
+     */
+    var isUndefined = malabi.isUndefined = function(item) {
+        if (typeof item === 'undefined') {
+            return true;
+        }
+        return false;
+    };
+
+    // TEST: ['a', null, 1, '1', undefined, [], {}, function() {}]
+    var isArray = malabi.isArray = function(item) {
+        if (Object.prototype.toString.call(item) == '[object Array]') {
+            return true;
+        }
+        return false;
+    };
+
+    // accepts numbers, not strings etc.
+    malabi.isNumber = function(n) {
+        return (typeof n === 'number' ? true : false);
+    };
+
+
+    // Accepts anything that converts to numbers, works: 1, '1', '1.2', '1E3', doesn't: false, NaN, '1f'
+    malabi.isNumeric = function(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    malabi.isString = function(item) {
+        if (typeof item === 'string') {
+            return true;
+        }
+        return false;
+    };
+
+    malabi.isEqual = function(a, b) {
+        var len, i, same = true;
+
+        if (a === b) {
+            return true;
+        // check array equality
+        } else if (isArray(a) && isArray(b)) {
+            if (a.length == b.length) {
+                len = a.length;
+                for (i = 0; i < len; i++) {
+                    same = (a[i] == b[i]);
+                    if (!same) break;
+                }
+                return same;
+            }
+        }
+        // TODO: compare objects
+
+        return false;
+    };
+})(this);
---
+++ test/malabi_spec.js
@@ -0,0 +1,329 @@
+var expect = chai.expect;
+
+describe("Malabi-library", function() {
+
+
+    describe("first", function() {
+        it("first([1, 2, 3]) should return 1", function() {
+            expect(malabi.first([1, 2, 3])).to.eql(1);
+        });
+    });
+
+    describe("last", function() {
+        it("last([1, 2, 3]) should return 3", function() {
+            expect(malabi.last([1, 2, 3])).to.eql(3);
+        });
+    });
+
+    describe("rest", function() {
+        it("rest([1, 2, 3]) should return [2, 3]", function() {
+            expect(malabi.rest([1, 2, 3])).to.eql([2, 3]);
+        });
+    });
+
+
+    describe("flatten", function() {
+        it("flatten([1, [2, [3]]]) should return [1, 2, 3]", function() {
+            expect(malabi.flatten([1, [2, [3]]])).to.eql([1, 2, 3]);
+        });
+
+        it("flatten([1, 2, 3]) should return [1, 2, 3]", function() {
+            expect(malabi.flatten([1, 2, 3])).to.eql([1, 2, 3]);
+        });
+    });
+
+    /**
+     * Functionalish-functions
+     */
+    describe("partialLeft", function() {
+        it("Makes a partial function with leftmost parameter", function() {
+            var myFunc = malabi.partialLeft(malabi.map, [1, 2, 3]);
+            expect(myFunc(function(item) { return item * 3; })).to.eql([3, 6, 9]);
+        });
+    });
+
+    describe("partialRight", function() {
+        it("Makes a partial function with second parameter", function() {
+            var myFunc = malabi.partialRight(malabi.map, function(item) { return item * 3; });
+            expect(myFunc([1, 2, 3])).to.eql([3, 6, 9]);
+        });
+    });
+
+
+    describe("map", function() {
+        it("map([1, 2, 3], function(value, index) { return value * 3; }) should return [3, 6, 9]", function() {
+            expect(malabi.map([1, 2, 3], function(a) { return a * 3; })).to.eql([3, 6, 9]);
+        });
+
+        it("map([1, 2, 3], function(value, index) { return index; }) should return [0, 1, 2]", function() {
+            expect(malabi.map([1, 2, 3], function(value, index) { return index; })).to.eql([0, 1, 2]);
+        });
+    });
+
+    describe("filter", function() {
+        it("filter([1, 2, 3], function(value, index) { return value * 3; }) should return [3, 6, 9]", function() {
+            expect(malabi.filter([1, 2, 3], function(a) { return (a % 2 === 1 ? true : false); })).to.eql([1, 3]);
+        });
+    });
+
+    describe("reduce", function() {
+        it("reduce([1, 2, 3], function(sum, value) { return sum + value; }) should return 6", function() {
+            var sum = function(sum, value) { return sum + value; };
+            expect(malabi.reduce([1, 2, 3], sum)).to.equal(6);
+        });
+
+        it("reduce([[0, 1], [2, 3], [4, 5]], function(a, b) { return a.concat(b); }) should return 6", function() {
+            var cat = function(a, b) { return a.concat(b); };
+            expect(malabi.reduce([[0, 1], [2, 3], [4, 5]], cat)).to.eql([0, 1, 2, 3, 4, 5]);
+        });
+    });
+
+
+    describe("foreach", function() {
+        it("Traverses all items", function() {
+            var ar = malabi.range(100),
+                inc = 0;
+
+            malabi.foreach(ar, function(value, index) {
+                expect(value).to.eql(index + 1);
+                inc += 1;
+            });
+            expect(inc).to.eql(ar.length);
+        });
+
+        it("Returns both value and index for mapping", function() {
+            var ar = [1, 2, 3];
+            malabi.foreach(ar, function(value, index) {
+                expect(value).to.eql(index + 1);
+            });
+        });
+    });
+
+    describe("pluck", function() {
+        var set = [
+                    {'foo':'bar', 'fox':'bag'},
+                    {'foo':'baz', 'foz':'bag'},
+                    {'foo':'bax', 'foy':'bag'}
+                ];
+        it("Should be able to fetch items with property name", function() {
+            expect(malabi.pluck(set, 'foo')).to.eql(['bar', 'baz', 'bax']);
+        });
+    });
+
+    describe("contains", function() {
+        var set = ['foo', 'bar', 'baz'];
+        it("Finds a value in an array ", function() {
+            expect(malabi.contains(set, 'baz')).to.equal(true);
+        });
+        it("Returns false is value is not found in the array ", function() {
+            expect(malabi.contains(set, 'bax')).to.equal(false);
+        });
+    });
+
+    describe("compose", function() {
+        // NOTE: should composition and testing for arguments usage be separate tests?
+        it("Should be able to compose two functions", function() {
+            var a = function(a) { return a + 1; };
+            var b = function(a) { return a + 2; };
+            var c = malabi.compose(a, b);
+
+            expect(c(1)).to.equal(4);
+        });
+    });
+
+    /**
+     * Array functions
+     */
+     // TODO: Add tests for First
+     // TODO: add tests for last
+     // TODO: add tests for rest
+     // TODO: add tests for flatten
+    describe("range", function() {
+        it("range(5) should return [1, 2, 3, 4, 5]", function() {
+            expect(malabi.range(5)).to.eql([1, 2, 3, 4, 5]);
+        });
+
+        it("range(0) should return []", function() {
+            expect(malabi.range(0)).to.eql([]);
+        });
+    });
+
+
+    /**
+     * Comparisons
+     */
+    describe("isEqual", function() {
+        it("Recognize equal arrays", function() {
+            expect(malabi.isEqual([1, 2], [1, 2])).to.eql(true);
+        });
+
+        it("Recognize unequal arrays", function() {
+            expect(malabi.isEqual([1, 2], [1, 3])).to.equal(false);
+        });
+
+        it("Empty array comparison", function() {
+            expect(malabi.isEqual([], [])).to.equal(true);
+        });
+
+        xit("Empty object comparison", function() {
+            expect(malabi.isEqual({}, {})).to.equal(true);
+        });
+
+        xit("Recognize similar objects", function() {
+            expect(malabi.isEqual({'foo':'bar'}, {'foo':'bar'})).to.equal(true);
+        });
+
+        xit("Recognize different objects (same keys, different values)", function() {
+            expect(malabi.isEqual({'foo':'bar'}, {'foo':'baz'})).to.equal(true);
+        });
+
+        it("isEqual('string', 'string') should return true", function() {
+            expect(malabi.isEqual('string', 'string')).to.equal(true);
+        });
+
+        it("isEqual(null, null) should return true", function() {
+            expect(malabi.isEqual(null, null)).to.equal(true);
+        });
+
+        it("isEqual(null, undefined) should return false", function() {
+            expect(malabi.isEqual(null, undefined)).to.equal(false);
+        });
+
+
+        it("isEqual(null, '') should return false", function() {
+            expect(malabi.isEqual(null, '')).to.equal(false);
+        });
+
+        it("isEqual(4, 4) should true", function() {
+            expect(malabi.isEqual(4, 4)).to.equal(true);
+        });
+    });
+
+
+    /**
+     * Type checks and conversions
+     */
+
+    describe("toArray", function() {
+        it("toArray(1, 2, 3) should equal [1, 2, 3]", function() {
+            expect(malabi.toArray(1, 2, 3)).to.eql([1, 2, 3]);
+        });
+    });
+
+    describe("isNumber", function() {
+        it("isNumber(1) to return true", function() {
+            expect(malabi.isNumber(1)).to.equal(true);
+        });
+        it("isNumber('1') to return false", function() {
+            expect(malabi.isNumber(1)).to.equal(true);
+        });
+        it("isNumber('string') to return false", function() {
+            expect(malabi.isNumber('string')).to.equal(false);
+        });
+        it("isNumber(undefined) to return false", function() {
+            expect(malabi.isNumber(undefined)).to.equal(false);
+        });
+        it("isNumber(null) to return false", function() {
+            expect(malabi.isNumber(null)).to.equal(false);
+        });
+    });
+
+    describe("isString", function() {
+        it("isString(1) should return false", function() {
+            expect(malabi.isString(1)).to.equal(false);
+        });
+
+        it("isString('1') should return true", function() {
+            expect(malabi.isString('1')).to.equal(true);
+        });
+    });
+
+
+    describe("isNumeric", function() {
+        it("isNumeric(1) to return true", function() {
+            expect(malabi.isNumeric(1)).to.equal(true);
+        });
+        it("isNumeric('1') to return true", function() {
+            expect(malabi.isNumeric('1')).to.equal(true);
+        });
+        it("isNumeric('string') to return false", function() {
+            expect(malabi.isNumeric('string')).to.equal(false);
+        });
+        it("isNumeric(undefined) to return false", function() {
+            expect(malabi.isNumeric(undefined)).to.equal(false);
+        });
+        it("isNumeric(null) to return false", function() {
+            expect(malabi.isNumeric(null)).to.equal(false);
+        });
+
+    });
+
+
+    // TEST: ['a', null, 1, '1', undefined, [], {}, function() {}]
+    describe("isArray", function() {
+        it("Should recognize an empty array", function() {
+            expect(malabi.isArray([])).to.equal(true);
+        });
+
+        it("Should recognize a non-empty array", function() {
+            expect(malabi.isArray([1])).to.equal(true);
+            expect(malabi.isArray([1, 2, 3, 'a'])).to.equal(true);
+        });
+
+        it("Should return false for undefined", function() {
+            expect(malabi.isArray(undefined)).to.equal(false);
+        });
+
+        it("Should return false for null", function() {
+            expect(malabi.isArray(null)).to.equal(false);
+        });
+
+        it("Should return false for strings", function() {
+            expect(malabi.isArray('string')).to.equal(false);
+        });
+
+        it("Should return false for objects", function() {
+            expect(malabi.isArray({})).to.equal(false);
+            expect(malabi.isArray({'joo':'ei'})).to.equal(false);
+        });
+
+        it("Should return false for function", function() {
+            expect(malabi.isArray(function() { return false; })).to.equal(false);
+        });
+    });
+
+
+    describe("isUndefined", function() {
+        it("Should return true for undefined", function() {
+            expect(malabi.isUndefined(undefined)).to.equal(true);
+        });
+
+        it("Should recognize return false on empty array", function() {
+            expect(malabi.isUndefined([])).to.equal(false);
+        });
+
+        it("Should recognize a non-empty array", function() {
+            expect(malabi.isUndefined([1])).to.equal(false);
+            expect(malabi.isUndefined([1, 2, 3, 'a'])).to.equal(false);
+        });
+
+        it("Should return false for null", function() {
+            expect(malabi.isUndefined(null)).to.equal(false);
+        });
+
+        it("Should return false for strings", function() {
+            expect(malabi.isUndefined('string')).to.equal(false);
+        });
+
+        it("Should return false for objects", function() {
+            expect(malabi.isUndefined({})).to.equal(false);
+            expect(malabi.isUndefined({'joo':'ei'})).to.equal(false);
+        });
+
+        it("Should return false for function", function() {
+            expect(malabi.isUndefined(function() { return false; })).to.equal(false);
+        });
+    });
+
+
+});
